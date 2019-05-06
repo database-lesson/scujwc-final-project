dbfbd144e47fdeee039cee6ad4cf428787713522
Vaskka <1139851358@qq.com>
2019-05-07 00:43:40 +0800

    first all sumn

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  min-width: 500px;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-leave {
+  opacity: 1;
+}
+
+.fade-enter-active {
+  transition: opacity 0.5s;
+}
+
+.fade-leave-active {
+  opacity: 0;
+  transition: opacity 0.5s;
+}
+</style>
